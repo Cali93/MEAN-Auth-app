c1731b0db96c9deb3e8baad040f5a3c13687d03f
Cali93 <c.armut93@outlook.com>
2018-01-09 18:41:46 +0100

    MEAN app ready do be deployed, builded in /public directory

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -15,13 +15,14 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
-  {path:'dashbord', component: DashbordComponent},
-  {path:'profile', component: ProfileComponent}
+  {path:'dashbord', component: DashbordComponent, canActivate:[AuthGuard]},
+  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
 ]
 @NgModule({
   declarations: [
@@ -40,7 +41,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule.forRoot()
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
